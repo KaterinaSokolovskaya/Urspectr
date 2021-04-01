26e54bd0892d1697ccb6b9fb8731a107e66fc02c
KaterinaSokolovskaya <ekaterina.sokol.20@yandex.com>
2021-04-01 18:04:54 +0300

    Version 1.0

--- src/main/java/com/sokolovskaya/urspectr/service/AverageService.java
+++ src/main/java/com/sokolovskaya/urspectr/service/AverageService.java
@@ -8,16 +8,25 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class AverageService {
 
     public Float getAverageNumberOfDocumentsInBackspin(BackspinCollection collection) {
-        Integer numberOfBackspins = collection.getBackspins().size();
+
+        BackspinCollection collectionNotDeleted = BackspinCollection.builder()
+                .backspins(collection.getBackspins()
+                        .stream()
+                        .filter(backspin -> backspin.getDeleted().equals(false))
+                        .collect(Collectors.toList()))
+                .build();
+
+        Integer numberOfBackspins = collectionNotDeleted.getBackspins().size();
 
         List<Document> documents = new ArrayList<>();
-        collection.getBackspins().forEach(backspin -> documents.addAll(backspin.getDocuments()));
+        collectionNotDeleted.getBackspins().forEach(backspin -> documents.addAll(backspin.getDocuments()));
 
         return Float.valueOf(documents.size()) / Float.valueOf(numberOfBackspins);
     }
